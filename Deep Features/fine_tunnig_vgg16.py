8833099917e3a2a534a847aeef68fe5cee23d7b2
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2018-11-01 10:58:29 -0300

    VGG
    Fine tunning - método 1

--- Deep Features/fine_tunnig_vgg16.py
+++ Deep Features/fine_tunnig_vgg16.py
@@ -1,11 +1,21 @@
 from keras.applications.vgg16 import VGG16
-from keras.datasets import load_svmlight_file
 from keras.applications.vgg16 import preprocess_input, decode_predictions
-from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model, Sequential
 from keras.layers import Dense, Activation
-from keras import optimizers
+from keras.optimizers import Adam
 import numpy as np
+#from keras.metrics import categorical_crossentropy
+#from keras.layers.normalization import BatchNormalization
+#from keras.layers.convolutional import *
+
+train_path = "train_base/train"
+test_path = "train_base/test"
+valid_path = "train_base/valid"
+
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(768, 574), classes=["melanoma", "normais"], batch_size=10)
+valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(768, 574), classes=["melanoma", "normais"], batch_size=4)
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(768, 574), classes=["melanoma", "normais"], batch_size=10)
 
 model_vgg16 = VGG16()
 
@@ -21,6 +31,6 @@ for layer in model.layers: #congela as camadas iniciais do modelo para que não 
 
 model.add(Dense(2, activation='softmax')) #adicionamos uma nova dense layer como softmax, para classificação entre melanoma e não melanoma
 
-model.compile(optimizers.Adam(lr=0.001), loss="categorical_crossentropy", metrics=['accuracy', 'precision', 'recall', 'f1'])
+model.compile(Adam(lr=.0001), loss="categorical_crossentropy", metrics=['accuracy', 'precision', 'recall', 'f1'])
 
-#model.fit_generator(train_batches, steps_per_epoch=4, validation_data=valid_batches, validation_steps=4, epochs=5, verbose=2)
+model.fit_generator(train_batches, steps_per_epoch=4, validation_data=valid_batches, validation_steps=4, epochs=5, verbose=2)
